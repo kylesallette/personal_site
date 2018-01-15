86b083d4efc82b29911517c2e04ba7b04a6e9cf0
kylesallette <kylesallette@gmail.com>
2018-01-15 16:59:13 -0700

    personal site

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -9,4 +9,10 @@ class HomepageTest < CapybaraTestCase
     assert_equal 200, page.status_code
   end
 
+  def test_error_message_works
+    visit '/poop'
+
+    assert 404, page.status_code
+  end
+
 end
